9185bebbeaa99ef0045119c69e405e2d93d598ef
q7man <48259711+q7man@users.noreply.github.com>
2019-03-11 23:12:46 +0300

    Update Unicode.java

--- lesson1/Unicode.java
+++ lesson1/Unicode.java
@@ -1,7 +1,6 @@
 public class Unicode {
     public static void main(String[] args) {
-        int uc = 9398;
-        char cu = (char) uc;
+        char cu = 9398;
         for (int j = 0; j < 780; j++) {
             System.out.print(cu);
             cu++;
