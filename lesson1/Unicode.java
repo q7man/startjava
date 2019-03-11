3b8a4c381ab36775c65e3064ccec22e73b304eef
q7man <48259711+q7man@users.noreply.github.com>
2019-03-11 22:04:13 +0300

    Update Unicode.java

--- lesson1/Unicode.java
+++ lesson1/Unicode.java
@@ -4,7 +4,7 @@ public class Unicode {
         char ca = '\u9398';
         for (int j = 0; j < 780; j++) {
             String sc = String.valueOf(ca);
-            System.out.print(sc + " ");
+            System.out.print(sc);
             ca++;
         }
     }
