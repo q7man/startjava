4c85cda1c6a7229ca7ed0708f1598e930976a3bd
q3man <q3man@inbox.ru>
2019-03-11 21:16:59 +0300

    дз

---
+++ lesson1/Unicode.java
@@ -0,0 +1,11 @@
+public class Unicode {
+    public static void main(String[] args) {
+
+        char ca = '\u9398';
+        for (int j = 0; j < 780; j++) {
+            String sc = String.valueOf(ca);
+            System.out.print(sc + " ");
+            ca++;
+        }
+    }
+}
