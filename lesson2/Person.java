1eab86934200107542bc682563083666bd7d50ca
q3man <q3man@inbox.ru>
2019-03-13 01:52:07 +0300

    дз

---
+++ lesson2/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    String sex = "male";
+    String name = "Hakuhoo";
+    int height = 197;
+    int weight = 180;
+    int age = 34;
+
+    String toTalk() {
+        String meet = "Hi";
+        return meet;
+    }
+
+    boolean toWalk() {
+        boolean walking = true;
+        return walking;
+    }
+
+    public void walkDistance(int rasstoyanie) {
+        rasstoyanie = 100;
+    }
+}
