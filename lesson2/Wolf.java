0295d6cf512f0db80da446c7fabe633a2682dbf8
q3man <q3man@inbox.ru>
2019-03-14 22:56:12 +0300

    дз 2,4

--- lesson2/Wolf.java
+++ lesson2/Wolf.java
@@ -1,29 +1,46 @@
 public class Wolf {
-    String sex = "male wolf";
-    String name = "PLuffy";
-    String color = "grey";
-    int weight = 10;
-    int ages = 5;
+    private String sex = "male wolf";
+    private String name = "PLuffy";
+    private String color = "grey";
+    private int weight = 10;
+    private int ages = 5;
 
-    int walking() {
+    public int setAges(int ages) {
+        if (ages > 8) {
+            System.out.println("Некорретный возраст");
+        } else {
+            this.ages = ages;
+        }
+        return ages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public int walking() {
         int walk = 10;
         return walk;
     }
 
-    boolean sit() {
+    public boolean sit() {
         return true;
     }
 
-    int run(int rasstoyanie) {
+    public int run(int rasstoyanie) {
         int runs = rasstoyanie * 2;
         return runs;
     }
 
-    String hawl() {
+    public String hawl() {
         return "woooo!";
     }
 
-    boolean hunt() {
+    public boolean hunt() {
         System.out.println("Pluffy loves hunting");
         return true;
     }
--- lesson2/WolfTest.java
+++ lesson2/WolfTest.java
@@ -1,8 +1,8 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolfy = new Wolf();
-        System.out.println(wolfy.name + " " + wolfy.sex);
-        System.out.println(wolfy.name + " love walkin " + wolfy.walking() + " hours, every day");
+        System.out.println(wolfy.getName() + " " + wolfy.setAges(9) + " years " + wolfy.getSex());
+        System.out.println(wolfy.getName() + " love walkin " + wolfy.walking() + " hours, every day");
         wolfy.run(100);
         wolfy.sit();
         System.out.println(wolfy.run(100) + "kms");
