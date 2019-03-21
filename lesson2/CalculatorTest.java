dd38ffe013a516d92fbd81110e6037d1656b914b
q3man <q3man@inbox.ru>
2019-03-21 18:46:05 +0300

    hw

--- lesson2/CalculatorTest.java
+++ lesson2/CalculatorTest.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
 public class CalculatorTest {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Calculator calc = new Calculator();
-        calc.message();
-        calc.mat();
-        calc.end();
+        System.out.println("Калькулятор!");
+        System.out.print("Введите первое число: ");
+        calc.firstNumber = scanner.nextInt();
+        System.out.print("Введите второе число: ");
+        calc.secondNumber = scanner.nextInt();
+        System.out.print("Введите знак математической операции: ");
+        calc.matOperation = scanner.nextLine();
+        calc.matLogic();
+        System.out.println("Если вы хотите продолжить: 1) да 2) нет");
+        calc.continueChoose = scanner.nextInt();
+        while (calc.continueChoose == 1) {
+            System.out.print("Введите первое число: ");
+            calc.firstNumber = scanner.nextInt();
+            System.out.print("Введите второе число: ");
+            calc.secondNumber = scanner.nextInt();
+            System.out.print("Введите знак математической операции: ");
+            calc.matOperation = scanner.nextLine();
+            calc.matLogic();
+            System.out.println("Если вы хотите продолжить: 1) да 2) нет");
+            calc.continueChoose = scanner.nextInt();
+        }
+        if (calc.continueChoose == 2) {
+            System.out.println("exit");
+        } else {
+            calc.continueChoose++;
+        }
     }
 }
